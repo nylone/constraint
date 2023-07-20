6304403caa01d2f00b13d026cb35827dac32af02
nylone <dev@rampeo.com>
2023-07-20 16:14:13 +0200

    feat(backend): added view support to model and controller

--- backend/controller/controller.go
+++ backend/controller/controller.go
@@ -3,18 +3,12 @@ package controller
 import (
 	"constraint/model"
 	"errors"
-)
-
-type Winner int8
-
-const (
-	NoWinner Winner = iota
-	Player1
-	Player2
+	"sync"
 )
 
 type Controller struct {
 	model model.Model
+	mutex sync.Mutex
 }
 
 func NewController(size uint) Controller {
@@ -23,17 +17,33 @@ func NewController(size uint) Controller {
 	}
 }
 
-func (controller Controller) isSafePos(pos model.Pos) bool {
-	return pos.X > 0 && pos.Y > 0 && pos.X < int(controller.model.Size) && pos.Y < int(controller.model.Size)
+func (controller *Controller) AddView(c chan<- (model.UpdateMessage)) {
+	// acquire lock on mutex
+	controller.mutex.Lock()
+	defer controller.mutex.Unlock()
+
+	controller.model.AddListener(c)
 }
 
-func (controller Controller) isMarkAdjacent(pos model.Pos) bool {
-	varX := controller.model.History[len(controller.model.History)-1].X - pos.X
-	varY := controller.model.History[len(controller.model.History)-1].Y - pos.Y
+func isSafePos(pos model.Pos, size uint) bool {
+	return pos.X > 0 && pos.Y > 0 && pos.X < int(size) && pos.Y < int(size)
+}
+
+func isMarkAdjacent(pos model.Pos, lastPos model.Pos) bool {
+	varX := lastPos.X - pos.X
+	varY := lastPos.Y - pos.Y
 	return varX < -1 || varX > 1 || varY < -1 || varY > 1
 }
 
-func (controller Controller) AddMark(pos model.Pos, mark model.Mark) error {
+func (controller *Controller) AddMark(pos model.Pos, mark model.Mark) error {
+	// acquire lock on mutex
+	controller.mutex.Lock()
+	defer controller.mutex.Unlock()
+
+	//check if game is over
+	if controller.model.CheckWinner() != model.NoWinner {
+		return errors.New("game over")
+	}
 	// special rules apply if it's the first round
 	if len(controller.model.History) == 0 {
 		// check to see if it's the correct turn
@@ -41,7 +51,7 @@ func (controller Controller) AddMark(pos model.Pos, mark model.Mark) error {
 			return errors.New("wrong turn")
 		}
 		// check to see if it's the position is safe
-		if !controller.isSafePos(pos) {
+		if !isSafePos(pos, controller.model.Size) {
 			return errors.New("unsafe position")
 		}
 		// finalize move
@@ -54,44 +64,14 @@ func (controller Controller) AddMark(pos model.Pos, mark model.Mark) error {
 		return errors.New("wrong turn")
 	}
 	// check to see if it's the position is safe
-	if !controller.isSafePos(pos) {
+	if !isSafePos(pos, controller.model.Size) {
 		return errors.New("unsafe position")
 	}
 	// check to see il move is adjacent the previous one
-	if controller.isMarkAdjacent(pos) {
+	if isMarkAdjacent(pos, controller.model.History[len(controller.model.History)-1]) {
 		return errors.New("invalid position")
 	}
 	// finalize move
 	controller.model.PutMark(pos, mark)
 	return nil
-}
-
-func (controller Controller) CheckWinner() Winner {
-	lastPos := controller.model.History[len(controller.model.History)-1]
-	posToCheck := [8]model.Pos{
-		{X: lastPos.X - 1, Y: lastPos.Y - 1},
-		{X: lastPos.X + 0, Y: lastPos.Y - 1},
-		{X: lastPos.X + 1, Y: lastPos.Y - 1},
-		{X: lastPos.X - 1, Y: lastPos.Y + 0},
-		{X: lastPos.X + 1, Y: lastPos.Y + 0},
-		{X: lastPos.X - 1, Y: lastPos.Y + 1},
-		{X: lastPos.X + 0, Y: lastPos.Y + 1},
-		{X: lastPos.X + 1, Y: lastPos.Y + 1},
-	}
-	for _, p := range posToCheck {
-		// check if p is out of bounds, if so skip it
-		if p.X < 0 || p.X > int(controller.model.Size) || p.Y < 0 || p.Y > int(controller.model.Size) {
-			continue
-		}
-		// p is guaranteed in bounds, check to see if it is occupied
-		if controller.model.Field[p.X][p.Y] == model.NoMark {
-			return NoWinner
-		}
-	}
-	// all positions are occupied, there is a winner
-	if controller.model.Field[lastPos.X][lastPos.Y] == model.P1Mark {
-		return Player2
-	} else {
-		return Player1
-	}
 }
--- backend/model/model.go
+++ backend/model/model.go
@@ -8,6 +8,20 @@ const (
 	P2Mark
 )
 
+type Winner int8
+
+const (
+	NoWinner Winner = iota
+	Player1
+	Player2
+)
+
+type UpdateMessage struct {
+	Pos    Pos
+	Mark   Mark
+	Winner Winner
+}
+
 type Field [][]Mark
 
 type Pos struct {
@@ -15,9 +29,10 @@ type Pos struct {
 }
 
 type Model struct {
-	Size    uint
-	Field   Field
-	History []Pos
+	Size      uint
+	Field     Field
+	History   []Pos
+	listeners []chan<- (UpdateMessage)
 }
 
 func NewModel(size uint) Model {
@@ -27,13 +42,60 @@ func NewModel(size uint) Model {
 	}
 
 	return Model{
-		Size:    size,
-		Field:   field,
-		History: make([]Pos, 0),
+		Size:  size,
+		Field: field,
 	}
 }
 
+func (model *Model) AddListener(c chan<- (UpdateMessage)) {
+	model.listeners = append(model.listeners, c)
+}
+
 func (model *Model) PutMark(p Pos, m Mark) {
+	// edit the model
 	model.Field[p.X][p.Y] = m
 	model.History = append(model.History, p)
+
+	// update the subscribed entities
+	message := UpdateMessage{
+		Pos:    p,
+		Mark:   m,
+		Winner: model.CheckWinner(),
+	}
+	for _, c := range model.listeners {
+		c <- message
+	}
+}
+
+func (model *Model) CheckWinner() Winner {
+	if len(model.History) == 0 {
+		return NoWinner
+	}
+	lastPos := model.History[len(model.History)-1]
+	posToCheck := [8]Pos{
+		{X: lastPos.X - 1, Y: lastPos.Y - 1},
+		{X: lastPos.X + 0, Y: lastPos.Y - 1},
+		{X: lastPos.X + 1, Y: lastPos.Y - 1},
+		{X: lastPos.X - 1, Y: lastPos.Y + 0},
+		{X: lastPos.X + 1, Y: lastPos.Y + 0},
+		{X: lastPos.X - 1, Y: lastPos.Y + 1},
+		{X: lastPos.X + 0, Y: lastPos.Y + 1},
+		{X: lastPos.X + 1, Y: lastPos.Y + 1},
+	}
+	for _, p := range posToCheck {
+		// check if p is out of bounds, if so skip it
+		if p.X < 0 || p.X > int(model.Size) || p.Y < 0 || p.Y > int(model.Size) {
+			continue
+		}
+		// p is guaranteed in bounds, check to see if it is occupied
+		if model.Field[p.X][p.Y] == NoMark {
+			return NoWinner
+		}
+	}
+	// all positions are occupied, there is a winner
+	if model.Field[lastPos.X][lastPos.Y] == P1Mark {
+		return Player2
+	} else {
+		return Player1
+	}
 }
