4d24a4a84c90c02788fa7a14dc4c42dbc98ed182
Rampeo <rampeo.mattone@gmail.com>
2023-07-24 12:00:42 +0200

    fix(backend): fixed out of bounds on checkwinner

--- backend/model/model.go
+++ backend/model/model.go
@@ -64,7 +64,7 @@ func (model *Model) CheckWinner() Winner {
 	}
 	for _, p := range posToCheck {
 		// check if p is out of bounds, if so skip it
-		if p.X < 0 || p.X > int(model.Size) || p.Y < 0 || p.Y > int(model.Size) {
+		if p.X < 0 || p.X >= int(model.Size) || p.Y < 0 || p.Y >= int(model.Size) {
 			continue
 		}
 		// p is guaranteed in bounds, check to see if it is occupied
