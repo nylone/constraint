f3ee4a0280ae97a8add1596309134868088f15df
slashtube <tommy.ferpet6@gmail.com>
2023-08-19 17:20:04 +0200

    feat(frontend): implemented handlers for OutputController, StartingInfo, NewClientInfo, JoinResponse and ClientLeft signals

--- frontend/src/routes/play/engine.svelte
+++ frontend/src/routes/play/engine.svelte
@@ -1,38 +1,106 @@
 <script>
     import { Table, TableBody, TableBodyCell, TableBodyRow } from 'flowbite-svelte'
+    import { Heading, P } from 'flowbite-svelte'
 
     // Props 
     export let lobby_id = ""
     export let nickname = ""
 
     // Variables
-    //let rows = Array.apply(null, Array(8)).map(function () {})
-    //let cols = Array.apply(null, Array(8)).map(function () {})
     let grid = Array.from(Array(8), () => new Array(8))
-    let id = 0
+    let ActionId = 0 
+    /* 
+        Sets the desired action:
+            0 - Add position. This is the default action
+            1 - Send message
+    */
+
+    let players = 1
+    let msg = ''
+    let error = false
+    let mark
+    let MatchOver = false
 
     // Connects to the socket
     let socket_url = `ws://localhost:8080/?lobby=${lobby_id}&nickname=${nickname}`
     const socket = new WebSocket(socket_url)
     
-    // Various functions
-    function send_move(x, y) {
-        socket.send(JSON.stringify({id: id, pos:{x: x, y: y}}))
-        id += 1
-        grid[y][x] = 'x'
+
+    // Events handling
+    socket.onmessage = function(e) {
+        // Gets the output signal
+        let signal = JSON.parse(e.data)
+        let signal_id = signal["id"]
+
+        // Handles the signals
+        switch(signal_id) {
+            case 0: // OutputController signal
+                if(!signal["successful"]) {
+                    msg = signal["error"]
+                    error = true
+                }
+                else {
+                    msg = ''
+                    error = false
+                }
+                break;
+            case 1: // StartingInfo signal
+                if(Object.keys(signal["players"]).length > 1) {
+                    players = Object.keys(signal["players"]).length
+                }
+                grid = signal["field"]
+                break;
+            case 2: // ModelUpdate signal
+                break;
+            case 3: // NewClientInfo signal
+                players++
+                mark = signal["mark"]
+                msg = `${signal["nickname"]} has connected to the lobby`
+                break;
+            case 4: // JoinResponse signal
+                if(!signal["successful"]) {
+                    msg = signal["error"]
+                    error = true
+                }
+                else {
+                    msg = 'Successfully connected'
+                    error = false
+                }
+                break;
+            case 5: // ClientLeft signal
+                MatchOver = signal["shutdown"]
+                msg = `${signal["nickname"]} has left the lobby`
+                break;
+            case 6: // ChatMessage signal
+                break;
+        }
     }
 
+
+    // Various functions
+    function send_move(x, y) {
+        if(ActionId === 0) {
+            socket.send(JSON.stringify({id: ActionId, pos:{x: x, y: y}}))
+            grid[y][x] = 'x'
+        }
+    }
 
 </script>
 
 <div>
+    {#if players < 2} 
+        <Heading tag="h4" class="mb-4 text-center">
+            Waiting for players ...
+        </Heading>
+    {/if}
+    <P> Players: {players} </P>
     <Table>
         <TableBody tableBodyClass="border-4">
             {#each grid as row, row_index}
                 <TableBodyRow>
                     {#each grid as col, col_index}
                     <TableBodyCell
-                        id={`col-${col_index+1}`} 
+                        id={`col-${col_index}`} 
                         tdClass="border px-6 py-4 whitespace-nowrap font-medium "
                         on:click={() => send_move(col_index, row_index)}
                     >
@@ -45,4 +113,15 @@
             {/each}
         </TableBody>
     </Table>
+    <Heading tag="h5">Console</Heading>
+    {#if msg != '' && !error} 
+        <P > {msg} </P> 
+    {:else if msg != '' && error}
+        <P color="text-red-700 dark:text-red-500"> {msg} </P>
+    {/if} 
+
+    {#if MatchOver} 
+        <Heading tag="h3"> Match is over </Heading>
+    {/if}
+
 </div>
